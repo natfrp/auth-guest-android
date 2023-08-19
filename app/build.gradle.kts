ce2b8dfcd05e9191fc699ea94dc9c051900e5622
bobo liu <liu@bobo.moe>
2023-08-19 21:17:00 +0800

    bump to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.natfrp.authguest"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         vectorDrawables {
             useSupportLibrary = true
