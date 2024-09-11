07e861e372a6aa17baf86313869027fd8eb82de6
bobo liu <liu@bobo.moe>
2024-09-11 23:40:03 +0800

    update deps and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.natfrp.authguest"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         vectorDrawables {
             useSupportLibrary = true
@@ -68,18 +68,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("net.nicbell.material-lists:listitem:0.1.1")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
     // QRCode Scan
     implementation("androidx.camera:camera-mlkit-vision:1.3.0-beta02")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    val cameraxVersion = "1.2.3"
+    val cameraxVersion = "1.3.4"
     implementation("androidx.camera:camera-core:${cameraxVersion}")
     implementation("androidx.camera:camera-camera2:${cameraxVersion}")
     implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
